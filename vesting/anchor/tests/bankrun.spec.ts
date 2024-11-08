3dddef0d7e8b055bda01bf5f8241defb8a030bfe
Abduh <mhmmdbduh@yahoo.com>
2024-11-08 23:07:22 +0800

    gd

---
+++ vesting/anchor/tests/bankrun.spec.ts
@@ -0,0 +1,44 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { ProgramTestContext, startAnchor } from "solana-bankrun";
+
+
+import IDL from "../target/idl/vesting.json";
+import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
+import { BankrunProvider } from "anchor-bankrun";
+import { Program } from "@coral-xyz/anchor";
+import { Vesting } from "@project/anchor";
+
+describe("Vesting Smart Contract Test", () => {
+    let beneficiary: Keypair;
+    let context: ProgramTestContext;
+    let provider: BankrunProvider; 
+    let program: anchor.Program<Vesting>; 
+    beforeAll ( async () => {
+        beneficiary = new anchor.web3.Keypair();
+
+        context = await startAnchor(
+            "",
+            [ {name: "vesting", programId: new PublicKey(IDL.address)},
+        ],
+    [
+        {
+            address: beneficiary.publicKey,
+            info: {
+                lamports: 1000_000_000,
+                data: Buffer.alloc(0),
+                owner: SYSTEM_PROGRAM_ID,
+                executable: false,
+            }
+        }
+    ])
+    
+        provider = new BankrunProvider(context);
+
+        anchor.setProvider(provider);
+
+        program = new Program<Vesting>(IDL as Vesting, provider),
+
+    });
+
+});
